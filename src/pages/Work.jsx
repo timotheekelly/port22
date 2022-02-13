73aeed86fbb1f069cfbb1b9af8660f713b14f82b
Tim Kelly <tkelly3118@gmail.com>
2022-02-13 21:07:14 +0000

    images

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Project from '../components/Project';
 
 import '../styles/Work.css';
-import aiImage from '/ai.jpg';
-import blockImg from '/block.jpg';
-import pinImg from '/pin.jpg';
-import cryptoImg from '/crypto.jpg';
-import ramenImg from '/ramen.jpg';
-import retroImg from '/retro.jpg';
+import aiImage from '../img/ai.jpg';
+import blockImg from '../img/block.jpg';
+import pinImg from '../img/pin.jpg';
+import cryptoImg from '../img/crypto.jpg';
+import ramenImg from '../img/ramen.jpg';
+import retroImg from '../img/retro.jpg';
 
 const Work = () => {
   return (
@@ -15,7 +15,7 @@ const Work = () => {
 
     <div className="projects">
       <Project title='Pinesque' background={pinImg} className='project'/>
-      <Project title='Web 3.0' background={blockImg} className='project'/>
+      <Project title='Web 3.0' background={process.env.PUBLIC_URL + "/block.jpg'"} className='project'/>
       <Project title='Neural Net' background={aiImage} className='project'/>
       <Project title='Retro' background={retroImg} className='project'/>
       <Project title='Ramen Shop' background={ramenImg} className='project'/>
